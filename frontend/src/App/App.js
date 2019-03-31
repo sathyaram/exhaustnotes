e70458162c6a862adb33d7ebcb0bea4d773756ff
Sathya Ram <sathyatheram@gmail.com>
2019-03-31 01:28:50 -0400

    player padding top decreased

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Header from '../Header/Header'
+import About from '../About/About'
 import CarGrid from '../CarGrid/CarGrid'
 import CarForm from '../CarForm/CarForm'
 import Footer from '../Footer/Footer'
-import About from '../About/About'
 import './App.scss';
-import { Route, Switch } from 'react-router-dom';
 
 
 class App extends Component {
@@ -13,12 +13,13 @@ class App extends Component {
   render() {
     return (
       <div className="page">
+        <a href="#main-content" class="invisible">Skip to main content</a>    
         <Header />
-        <main id="main-content" role="main">
+        <main role="main" id="main-content">
           <Switch>
-              <Route path="/submit" render={() => <CarForm />}/>
-              <Route path="/about" render={() => <About />}/>
-              <Route path="/" render={() => <CarGrid />}/>
+            <Route path="/submit" render={() => <CarForm />}/>
+            <Route path="/about" render={() => <About />}/>
+            <Route path="/" render={() => <CarGrid />}/>
           </Switch>
         </main>
         <Footer />
