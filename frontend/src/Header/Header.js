e2d6e84a5134df30eab37f36ea6ab6a238eb24e5
Sathya Ram <sathyatheram@gmail.com>
2019-04-21 19:04:24 -0400

    Fixed header tagline

--- frontend/src/Header/Header.js
+++ frontend/src/Header/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     return (
       <header id="header" role="banner">
         <h1 name="site-name" title="Exhaust Notes"><a href="/">Exhaust<br/>Notes</a></h1>
-        <h2 name="tagline">A Collection of Car Engine Sounds</h2>
+        <h2 name="tagline">A Car Engine Sound Library</h2>
       </header>
     );
   }
