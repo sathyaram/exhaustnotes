0e0af4072befebf2b7a875c510b2b15a9b647eab
andretimar <andre@andretimar.com>
2019-04-29 22:48:20 +0200

    Introduced meta tags

---
+++ frontend/src/config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  seo: {
+    title: 'ExhaustNotes | A Car Engine Sound Library',
+    description: 'A Car Engine Sound Library',
+    baseUrl: 'https://https://exhaustnotes.co',
+    image: 'https://www.allstarfordpalestine.com/inventoryphotos/5716/1fa6p8cf5k5106105/sp/1.jpg?width=2000'
+  }
+};
