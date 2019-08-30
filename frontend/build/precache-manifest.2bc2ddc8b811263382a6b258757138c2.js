6d89676af73e49fda113a52e42db2b2fdd5f06e5
Sathya Ram <sathyatheram@gmail.com>
2019-08-29 21:26:47 -0400

    Modified git ignore and added /build directory

---
+++ frontend/build/precache-manifest.2bc2ddc8b811263382a6b258757138c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ca3babecc2f832136e3",
+    "url": "/static/js/main.b0347311.chunk.js"
+  },
+  {
+    "revision": "1dcdf0c07b86006dc395",
+    "url": "/static/js/2.bd388492.chunk.js"
+  },
+  {
+    "revision": "7ca3babecc2f832136e3",
+    "url": "/static/css/main.deb3224c.chunk.css"
+  },
+  {
+    "revision": "aa6eedc48996b9df5f732f876eacece6",
+    "url": "/index.html"
+  }
+];
