ad2243fac798a83b859fff8afd1214e09e2a4360
Sathya Ram <sathyatheram@gmail.com>
2019-10-06 11:55:18 -0400

    New build fixing search text positioning

--- frontend/src/Search/Search.js
+++ frontend/src/Search/Search.js
@@ -8,7 +8,7 @@ class Search extends Component {
       <div className="carSearch">
         <div className="inputWrapper">
           <label>Car Search</label>
-          <input name="search" aria-label="search" id="search" type="text" placeholder="Type a Car or Scroll a While" role="search" onChange={this.props.updateQuery} value={this.props.searchQuery}/>
+          <input name="search" aria-label="search" id="search" type="text" placeholder="Type a Car or Scroll" role="search" onChange={this.props.updateQuery} value={this.props.searchQuery}/>
           <button name="clear search" id="clear-search" onClick={this.props.clearQuery}><i className="far fa-trash-alt"></i></button>
         </div>
       </div>
